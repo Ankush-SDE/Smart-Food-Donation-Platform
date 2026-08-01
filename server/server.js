783d599f304fe7750f960da9beee3b0ef7eaf8d5
Ankush Kumar <ankush1524kumar@gmail.com>
2026-08-01 19:52:17 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Smart Food Donation API is Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
