d7af25e536e10a887aace86446c1b44c3373146a
Ankush Kumar <ankush1524kumar@gmail.com>
2026-08-02 20:00:39 +0530

    update controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,50 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+// Register User
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+
+        // Check required fields
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        // Check existing user
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            });
+        }
+
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            role
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User Registered Successfully",
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const app = express();
